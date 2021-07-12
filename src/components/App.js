2bbb67ff8bdcb3e8fe47ab342b2c3a24217e1222
Frenzoid <frenzoid@protonmail.com>
2021-07-12 20:56:47 +0200

    created frontend with react and wallet connection with web3

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-//import DStorage from '../abis/DStorage.json'
+import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
 import Navbar from './Navbar'
 import Main from './Main'
 import Web3 from 'web3';
 import './App.css';
 
-//Declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
 
 class App extends Component {
 
@@ -15,69 +16,125 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    //Setting up Web3
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
   }
 
   async loadBlockchainData() {
-    //Declare Web3
+    const web3 = window.web3
 
-    //Load account
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    const balance = (await web3.eth.getBalance(accounts[0]) / 100000000000000000).toFixed(2)
+    this.setState({ account: accounts[0] })
+    this.setState({ balance: balance })
 
-    //Network ID
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DStorage.networks[networkId]
+    if (networkData) {
 
-    //IF got connection, get data from contracts
-      //Assign contract
+      // Assign contract
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+      this.setState({ dstorage })
 
-      //Get files amount
+      // Get files amount
+      const filesCount = await dstorage.methods.fileCount().call()
+      this.setState({ filesCount })
 
-      //Load files&sort by the newest
-
-    //Else
-      //alert Error
-
+      // Load files&sort by the newest
+      for (var i = filesCount; i >= 1; i--) {
+        const file = await dstorage.methods.files(i).call()
+        this.setState({
+          files: [...this.state.files, file]
+        })
+      }
+    } else {
+      window.alert('DStorage contract not deployed to detected network.')
+    }
   }
 
   // Get file from user
   captureFile = event => {
+    event.preventDefault()
+
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result),
+        type: file.type,
+        name: file.name
+      })
+      console.log('buffer', this.state.buffer)
+    }
   }
 
-
-  //Upload File
   uploadFile = description => {
+    console.log("Submitting file to IPFS...")
 
-    //Add file to the IPFS
+    // Add file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result.size)
+      if (error) {
+        console.error(error)
+        return
+      }
 
-      //Check If error
-        //Return error
-
-      //Set state to loading
-
-      //Assign value for the file without extension
-
-      //Call smart contract uploadFile function 
-
+      this.setState({ loading: true })
+      // Assign value for the file without extension
+      if (this.state.type === '') {
+        this.setState({ type: 'none' })
+      }
+      this.state.dstorage.methods.uploadFile(result[0].hash, result[0].size, this.state.type, this.state.name, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({
+          loading: false,
+          type: null,
+          name: null
+        })
+        window.location.reload()
+      }).on('error', (e) => {
+        window.alert('Error')
+        this.setState({ loading: false })
+      })
+    })
   }
 
-  //Set states
   constructor(props) {
     super(props)
     this.state = {
+      account: '',
+      dstorage: null,
+      files: [],
+      loading: false,
+      type: null,
+      name: null
     }
-
-    //Bind functions
+    this.uploadFile = this.uploadFile.bind(this)
+    this.captureFile = this.captureFile.bind(this)
   }
 
   render() {
     return (
       <div>
-        <Navbar account={this.state.account} />
-        { this.state.loading
+        <Navbar account={this.state.account} balance={this.state.balance} />
+        {this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-              files={this.state.files}
-              captureFile={this.captureFile}
-              uploadFile={this.uploadFile}
-            />
+            files={this.state.files}
+            captureFile={this.captureFile}
+            uploadFile={this.uploadFile}
+          />
         }
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,10 +14,33 @@ class Navbar extends Component {
           rel="noopener noreferrer"
         >
           <img src={box} width="30" height="30" className="align-top" alt="" />
-          Multibox
+          Nodebox
         </a>
         <ul className="navbar-nav px-3">
-          <b className='text-white'>{'0x0'}</b>
+          <li>
+            <small id="account">
+              <span class="text-warning mr-2">
+                Balance: {(this.props.balance)} ETH
+              </span>
+              <a target="_blank"
+                alt=""
+                className="text-primary"
+                rel="noopener noreferrer"
+                href={"https://etherscan.io/address/" + this.props.account}>
+                {this.props.account.substring(0, 6)}...{this.props.account.substring(38, 42)}
+              </a>
+            </small>
+            {this.props.account
+              ? <img
+                alt=""
+                className='ml-2'
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+              />
+              : <span></span>
+            }
+          </li>
         </ul>
       </nav>
     );
