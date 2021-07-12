e23ba8ea2bed37bdba631d61eeb7fd6f8a394519
Frenzoid <frenzoid@protonmail.com>
2021-07-12 20:58:06 +0200

    remove unnecesary comments

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -89,22 +89,4 @@ contract DStorage {
             msg.sender
         );
     }
-
-    // Make sure the file hash exists
-
-    // Make sure file type exists
-
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-    // Increment file id
-
-    // Add File to the contract
-
-    // Trigger an event
 }
